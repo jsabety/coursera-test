63b091811d0512cca8a3f24cdc9cd30a6670ae8f
Jerome Sabety <jsabety@gmail.com>
2020-03-05 00:33:09 +0100

    Bindings part two

--- app.js
+++ app.js
@@ -3,15 +3,37 @@
 
  var x="hello";
 
-angular.module('myFirstApp', [])
+ var MyFirstApp = angular.module('myFirstApp', [])
 
-.controller('MyFirstController', function ($scope) {
+ MyFirstApp.controller('MyFirstController', function ($scope) {
     $scope.name = "Jerome";
     $scope.sayHello = function () {
         return "Hello Coursera";
     };
+});
+
+MyFirstApp.controller('NameCalculatorController', function ($scope) {
+    $scope.name = "";
+    $scope.totalValue = 0;
+
+    $scope.displaynumeric = function () {
+        var totalNameValue = calculateNumericForString($scope.name); //get total value of characters
+        $scope.totalValue = totalNameValue;
+    }
+
+
+
+    function calculateNumericForString(string) {
+        var totalStringValue = 0;
+        for (var i = 0; i < string.length; i++) {
+            totalStringValue += string.charCodeAt(i);
+        }
+        return totalStringValue;
+
+    }
 
 });
+
 
 })();
 
